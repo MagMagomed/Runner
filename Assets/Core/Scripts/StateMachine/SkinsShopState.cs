d0ada3dabaac6da9e57875324f9a4597db29db4a
LAPTOP-F0NQ82JO\mahen <mahendropolos@yandex.ru>
2023-09-23 01:04:38 +0300

    Переименовываю название поля

--- Assets/Core/Scripts/StateMachine/SkinsShopState.cs
+++ Assets/Core/Scripts/StateMachine/SkinsShopState.cs
@@ -6,20 +6,20 @@ using UnityEngine;
 
 public class SkinsShopState : AbstractState
 {
-    readonly Action m_OnPauseShopOpen;
+    readonly Action m_onSkinsShopOpen;
 
     public override string Name => $"{nameof(SkinsShopState)}";
 
     /// <param name="onPause">The action that is invoked when the game loop paused</param>
     public SkinsShopState(Action onPause)
     {
-        m_OnPauseShopOpen = onPause;
+        m_onSkinsShopOpen = onPause;
     }
 
     public override void Enter()
     {
         Time.timeScale = 0f;
-        m_OnPauseShopOpen?.Invoke();
+        m_onSkinsShopOpen?.Invoke();
     }
     public override IEnumerator Execute()
     {
